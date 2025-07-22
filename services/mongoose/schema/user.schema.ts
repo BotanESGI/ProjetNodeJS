11f09c563c130f3e7410d16ac9e29fb4487245c2
Aya Hanine <aya@MacBook-Air-de-Aya.local>
2025-07-22 11:19:01 +0200

    add attribut invitations to manage social challenge invitations

--- services/mongoose/schema/user.schema.ts
+++ services/mongoose/schema/user.schema.ts
@@ -8,7 +8,12 @@ export const userSchema = new mongoose.Schema({
     role:     { type: String, enum: Object.values(UserRole), required: true },
     isActive: { type: Boolean, default: true },
     badges:   [{ type: mongoose.Schema.Types.ObjectId, ref: 'Badge' }],
-    rewards:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'Reward' }] // 👈 AJOUT
+    rewards:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'Reward' }],
+    invitations: [{
+        challengeId: { type: mongoose.Schema.Types.ObjectId, ref: 'Challenge', required: true },
+        status: { type: String, enum: ["pending", "accepted", "declined"], default: "pending" },
+        invitedAt: { type: Date, default: Date.now }
+    }]
 
 }, { timestamps: true });
 
